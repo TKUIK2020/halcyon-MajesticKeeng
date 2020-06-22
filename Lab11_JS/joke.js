ceaa35f7a8956a4ac800aa13be535138f551c78b
MajesticKeeng <eph.sasha@gmail.com>
2020-06-22 14:29:36 +0300

    none

--- Lab11_JS/joke.js
+++
@@ -1,8 +0,0 @@
-const joker = require('random-joke-getter')
-
-require("http").createServer((req, res) => {
-    console.log(`Запрашиваемый url: ${req.url}`)
-    joker.getProgrammingJoke((joke) => {
-        res.end(joke)
-    })
-}).listen(3000, () => console.log("Server is on"));
